ac6489d388ac7a01b294e89824c3a0c5130614bf
oralkh <oralhan.seilhan07@gmail.com>
2026-02-16 08:38:43 +0500

    fix(crud): get create nodes

--- internal/model/node.go
+++ internal/model/node.go
@@ -1,6 +1,7 @@
 package model
 
 import (
+	"errors"
 	"time"
 
 	"go.mongodb.org/mongo-driver/bson/primitive"
@@ -27,4 +28,44 @@ type Node struct {
 	CreatedAt time.Time          `bson:"created_at"`
 	UpdatedAt *time.Time         `bson:"updated_at"`
 	DeletedAt *time.Time         `bson:"deleted_at"`
+}
+
+func NewNode(mapId, parentId, types, label string, x, y, w, h float64,
+	createdAt time.Time, updatedAt, deletedAt *time.Time) (*Node, error) {
+	if mapId == "" {
+		return nil, errors.New("map id is required")
+	} else if types == "" {
+		return nil, errors.New("types is required")
+	} else if w <= 0.0 {
+		return nil, errors.New("x is required")
+	} else if h <= 0.0 {
+		return nil, errors.New("y is required")
+	}
+	position := Point{X: x, Y: y}
+	data := NodeData{Label: label}
+
+	mapOId, err := primitive.ObjectIDFromHex(mapId)
+	if err != nil {
+		return nil, errors.New("invalid map id: " + mapId)
+	}
+	var parentOId primitive.ObjectID
+	if parentId != "" {
+		parentOId, err = primitive.ObjectIDFromHex(parentId)
+		if err != nil {
+			return nil, errors.New("invalid parent id: " + parentId)
+		}
+	}
+
+	return &Node{
+		MapID:     mapOId,
+		ParentID:  parentOId,
+		Type:      types,
+		Position:  position,
+		Data:      data,
+		Width:     w,
+		Height:    h,
+		CreatedAt: createdAt,
+		UpdatedAt: updatedAt,
+		DeletedAt: deletedAt,
+	}, nil
 }
